1e005e75238250ca2d505a10a0e189d5e34d0f33
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2024-06-12 19:40:31 +0530

    Update DBUtil.java

--- Login-Register/src/main/java/com/iss/util/DBUtil.java
+++ Login-Register/src/main/java/com/iss/util/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
 	
 	private static final String URL = "jdbc:mysql://localhost:3306/iss";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "Singh123@";
+	private static final String PASSWORD = "password";  // change password
 	
     static {
         try {
